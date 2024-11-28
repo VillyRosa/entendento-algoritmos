89ec1829c77a98a31b2964c03dc68d6d5cf1fc16
Villy Rosa <villyrosa@hotmail.com>
2024-11-28 13:43:10 -0300

    feat: add sixth chapter

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,5 @@
-import capitulo5.TabelasHash;
-import capitulo5.ListaTelefonica;
-import capitulo5.VerificaEleitor;
-
 public class Main {
     public static void main(String[] args) {
-        TabelasHash th = new TabelasHash();
-        th.exploraDicionario();
 
-        ListaTelefonica lt = new ListaTelefonica();
-        lt.adicionaContato("João", "1234-5678");
-        lt.adicionaContato("Maria", "8765-4321");
-        System.out.println(lt.buscaContato("João"));
-
-        VerificaEleitor ve = new VerificaEleitor();
-        ve.verificaEleitor("João");
-        ve.verificaEleitor("Maria");
-        ve.verificaEleitor("João");
     }
 }
